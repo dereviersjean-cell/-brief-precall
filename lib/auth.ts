fcb249df1338465bfb878bc606ed06bb62ab1353
Jean de Reviers <jean@MacBook-Air-2.local>
2026-06-29 15:12:57 +0200

    Auth: ajout scope gmail.send pour envoi email de suivi

--- lib/auth.ts
+++ lib/auth.ts
@@ -17,6 +17,7 @@ export const authOptions: AuthOptions = {
             "profile",
             "https://www.googleapis.com/auth/calendar.readonly",
             "https://www.googleapis.com/auth/gmail.readonly",
+            "https://www.googleapis.com/auth/gmail.send",
           ].join(" "),
           access_type: "offline",
         },
